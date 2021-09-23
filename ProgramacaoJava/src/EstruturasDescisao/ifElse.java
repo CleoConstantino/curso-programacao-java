7c02af3610ff2e2ea2a02aa7783c28dc07772b18
Cléo Constantino <cleoconstantino@outlook.com>
2021-09-22 21:28:29 -0300

    Add files via upload

---
+++ ProgramacaoJava/src/EstruturasDescisao/ifElse.java
@@ -0,0 +1,22 @@
+//if, else, else if
+package EstruturasDescisao;
+
+public class ifElse {
+
+	public static void main(String[] args) {
+		//Declarando e inicializando uma variável;
+		int numero = 5;
+		
+		if(numero > 5) {
+			System.out.println("Sim, o número " + numero + " é maior que 5!");
+		}
+		else if (numero == 5){
+			System.out.println("O número " + numero + " é igual 5!");
+		}
+		else {
+			System.err.println("Não, o número " + numero + " não é maior de 5!");
+		}
+
+	}
+
+}
